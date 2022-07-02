558fb56d65c4746a6b9b6c2110f37ea99f184e8c
PatriceBitonga <p.bitonga@gmail.com>
2022-07-01 18:38:34 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const shareBtn = document.querySelector('.share-icon');
+const socMed = document.querySelector('.soc-med');
 
-
-  
+shareBtn.addEventListener('click', () => {
+    socMed.classList.toggle('hidden');
+});
